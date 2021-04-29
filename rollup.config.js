d093a9b7745b41e95a01aa6e031f9d0115724a50
Magatte BA <magattestg@gmail.com>
2021-04-29 19:56:17 +0200

    (+) Configuring for npm

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+import packageJSON from './package.json';
+const input = './src/index.js';
+
+export default [
+    // CommonJS
+    {
+        input,
+        output: {
+            file: packageJSON.main,
+            format: 'cjs'
+        },
+        plugins: [
+            babel({
+                exclude: 'node_modules/**',
+                babelHelpers: 'runtime'
+            }),
+            resolve(),
+            commonjs()
+        ],
+        external: [/@babel\/runtime/]
+    }
+];
